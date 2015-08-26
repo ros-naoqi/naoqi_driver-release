a882046b249811eadc7528a7b0df774d3d8961db
Karsten Knese <kknese@aldebaran.com>
2015-08-26 14:18:48 +0200

    Rebase from 'debian/indigo/naoqi_driver'

--- src/helpers/driver_helpers.cpp
+++ src/helpers/driver_helpers.cpp
@@ -26,10 +26,17 @@ namespace driver
 
 /** Function that returns the type of a robot
  */
-static robot::Robot robot( const qi::SessionPtr& session)
+static naoqi_bridge_msgs::RobotInfo& getRobotInfoLocal( const qi::SessionPtr& session)
 {
-  robot::Robot bot;
+  static naoqi_bridge_msgs::RobotInfo info;
+  static qi::SessionPtr session_old;
 
+  if (session_old == session)
+    return info;
+
+  session_old = session;
+
+  // Get the robot type
   std::cout << "I am going to call RobotModel proxy" << std::endl;
   qi::AnyObject p_memory = session->service("ALMemory");
   std::string robot = p_memory.call<std::string>("getData", "RobotConfig/Body/Type" );
@@ -37,27 +44,142 @@ static robot::Robot robot( const qi::SessionPtr& session)
 
   if (std::string(robot) == "nao")
   {
-    bot = robot::NAO;
-    return bot;
+    info.type = naoqi_bridge_msgs::RobotInfo::NAO;
   }
   if (std::string(robot) == "pepper" || std::string(robot) == "juliette" )
   {
-    bot = robot::PEPPER;
-    return bot;
+    info.type = naoqi_bridge_msgs::RobotInfo::PEPPER;
   }
-  else
+  if (std::string(robot) == "romeo" )
   {
-    return robot::UNIDENTIFIED;
+    info.type = naoqi_bridge_msgs::RobotInfo::ROMEO;
   }
+
+  // Get the data from RobotConfig
+  qi::AnyObject p_motion = session->service("ALMotion");
+  std::vector<std::vector<qi::AnyValue> > config = p_motion.call<std::vector<std::vector<qi::AnyValue> > >("getRobotConfig");
+
+  // TODO, fill with the proper string matches from http://doc.aldebaran.com/2-1/naoqi/motion/tools-general-api.html#ALMotionProxy::getRobotConfig
+
+  for (size_t i=0; i<config[0].size();++i)
+  {
+    if (config[0][i].as<std::string>() == "Model Type")
+    {
+      try{
+        info.model = config[1][i].as<std::string>();
+      }
+      catch(const std::exception& e)
+      {
+        std::cout << "Error in robot config variable " << (config[0][i]).as<std::string>() << std::endl;
+      }
+    }
+
+    if (config[0][i].as<std::string>() == "Head Version")
+    {
+      try{
+        info.head_version = config[1][i].as<std::string>();
+      }
+      catch(const std::exception& e)
+      {
+        std::cout << "Error in robot config variable " << (config[0][i]).as<std::string>() << std::endl;
+      }
+    }
+
+    if (config[0][i].as<std::string>() == "Body Version")
+    {
+      try{
+        info.body_version = config[1][i].as<std::string>();
+      }
+      catch(const std::exception& e)
+      {
+        std::cout << "Error in robot config variable " << (config[0][i]).as<std::string>() << std::endl;
+      }
+    }
+
+    if (config[0][i].as<std::string>() == "Arm Version")
+    {
+      try{
+        info.arm_version = config[1][i].as<std::string>();
+      }
+      catch(const std::exception& e)
+      {
+        std::cout << "Error in robot config variable " << (config[0][i]).as<std::string>() << std::endl;
+      }
+    }
+
+    if (config[0][i].as<std::string>() == "Laser")
+    {
+      try{
+        info.has_laser = config[1][i].as<bool>();
+      }
+      catch(const std::exception& e)
+      {
+        std::cout << "Error in robot config variable " << (config[0][i]).as<std::string>() << std::endl;
+      }
+    }
+
+    if (config[0][i].as<std::string>() == "Extended Arms")
+    {
+      try{
+        info.has_extended_arms = config[1][i].as<bool>();
+      }
+      catch(const std::exception& e)
+      {
+        std::cout << "Error in robot config variable " << (config[0][i]).as<std::string>() << std::endl;
+      }
+    }
+
+    if (config[0][i].as<std::string>() == "Number of Legs")
+    {
+      try{
+        info.number_of_legs = config[1][i].as<int>();
+      }
+      catch(const std::exception& e)
+      {
+        std::cout << "Error in robot config variable " << (config[0][i]).as<std::string>() << std::endl;
+      }
+    }
+
+    if (config[0][i].as<std::string>() == "Number of Arms")
+    {
+      try{
+        info.number_of_arms = config[1][i].as<int>();
+      }
+      catch(const std::exception& e)
+      {
+        std::cout << "Error in robot config variable " << (config[0][i]).as<std::string>() << std::endl;
+      }
+    }
+
+    if (config[0][i].as<std::string>() == "Number of Hands")
+    {
+      try{
+        info.number_of_hands = config[1][i].as<int>();
+      }
+      catch(const std::exception& e)
+      {
+        std::cout << "Error in robot config variable " << (config[0][i]).as<std::string>() << std::endl;
+      }
+    }
+
+  }
+  return info;
 }
 
-const robot::Robot& getRobot( const qi::SessionPtr& session )
+robot::Robot getRobot( const qi::SessionPtr& session )
 {
-  static const robot::Robot r = robot(session);
-  return r;
+  // TODO: return the type from naoqi_bridge_msgs::RobotInfo
+  if ( getRobotInfo(session).type == naoqi_bridge_msgs::RobotInfo::NAO )
+    return robot::NAO;
+  if ( getRobotInfo(session).type == naoqi_bridge_msgs::RobotInfo::PEPPER )
+    return robot::PEPPER;
+}
+
+const naoqi_bridge_msgs::RobotInfo& getRobotInfo( const qi::SessionPtr& session )
+{
+  return getRobotInfoLocal(session);
 }
 
 } // driver
 } // helpers
 } // naoqi
-
